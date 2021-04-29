7ad0c6b469d80d8d94a2790def0cf16774b2ef49
Artyom Sovetnikov <elringus@gmail.com>
2021-04-29 16:27:31 +0300

    Initial commit

---
+++ Assets/Runtime/InitializeWithUniTask.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+using Naninovel;
+using UnityEngine;
+
+public class InitializeWithUniTask : MonoBehaviour
+{
+    // ReSharper disable once Unity.IncorrectMethodSignature
+    private async UniTaskVoid Start ()
+    {
+        await RuntimeInitializer.InitializeAsync();
+    }
+}
---
+++ Assets/Runtime/UniTaskCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Naninovel;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+[CommandAlias("uniTask")]
+public class UniTaskCommand : Command
+{
+    [ParameterAlias(NamelessParameterAlias), RequiredParameter]
+    public StringParameter Message;
+
+    public override async UniRx.Async.UniTask ExecuteAsync (CancellationToken cancellationToken = default)
+    {
+        var message = await WaitAndReturnMessageAsync();
+        Debug.Log(message);
+    }
+
+    private async UniTask<string> WaitAndReturnMessageAsync ()
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(1));
+        return Message;
+    }
+}
